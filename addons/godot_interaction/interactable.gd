dc09c06078b66da746308eabf247afa843a98477
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-08-03 22:23:54 +0200

    Add Interactable

---
+++ addons/godot_interaction/interactable.gd
@@ -0,0 +1,14 @@
+extends Area3D
+
+class_name Interactable
+
+# Emitted when an Interactor starts looking at object
+signal focused(interactor: Interactor)
+# Emitted when an Interactor stops looking at object
+signal unfocused(interactor: Interactor)
+# Emitted when an Interactor interacts with an object
+signal interacted(interactor: Interactor)
+# Emitted when an object is the closest to the Interactor
+signal closest(interactor: Interactor)
+# Emitted when an object is no longer the closest one to the Interactor
+signal not_closest(interactor: Interactor)
