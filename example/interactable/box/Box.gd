ed2b70c088f04e17bb6516cc1aea65d4464ba304
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-08-04 15:49:27 +0200

    Updated Box

--- example/interactable/box/Box.gd
+++ example/interactable/box/Box.gd
@@ -1,15 +1,19 @@
 extends Interactable
 
 @onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
+@onready var status: Label3D = $Status
 
 
 func _on_focused(_interactor: Interactor) -> void:
 	mesh_instance_3d.mesh.material.albedo_color = Color("red")
+	status.text = "Focused"
 
 
 func _on_unfocused(_interactor: Interactor) -> void:
 	mesh_instance_3d.mesh.material.albedo_color = Color("white")
+	status.text = "Unfocused"
 
 
 func _on_interacted(_interactor: Interactor) -> void:
 	mesh_instance_3d.rotate_x(45)
+	status.text = "Interacted"
