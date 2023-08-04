2208141a6169d2003d7784871127ecefcb838d19
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-08-04 13:32:28 +0200

    Update Box.gd

--- example/interactable/box/Box.gd
+++ example/interactable/box/Box.gd
@@ -3,13 +3,13 @@ extends Interactable
 @onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
 
 
-func _on_focused(_interactor) -> void:
+func _on_focused(_interactor: Interactor) -> void:
 	mesh_instance_3d.mesh.material.albedo_color = Color("red")
 
 
-func _on_unfocused(_interactor) -> void:
+func _on_unfocused(_interactor: Interactor) -> void:
 	mesh_instance_3d.mesh.material.albedo_color = Color("white")
 
 
-func _on_interacted(_interactor) -> void:
+func _on_interacted(_interactor: Interactor) -> void:
 	mesh_instance_3d.rotate_x(45)
