8952d1548e7bfc0baf26b1a2bea141079e33e1dd
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-08-10 15:11:11 +0200

    Working Example 3

--- examples/scenes/example3/interactable/interactable_prop/InteractableProp.gd
+++ examples/scenes/example3/interactable/interactable_prop/InteractableProp.gd
@@ -1,5 +1,16 @@
 extends InteractableProp
 
+@onready var text: Label3D = $Text
 
-func _ready() -> void:
-	super()
+
+func _focused(_interactor: Interactor) -> void:
+	text.text = "Focused"
+
+
+func _unfocused(_interactor: Interactor) -> void:
+	text.text = "Unfocused"
+
+
+func _interacted(_interactor: Interactor) -> void:
+	text.text = "Interacted"
+	mesh_instance_3d.rotate_z(45)
