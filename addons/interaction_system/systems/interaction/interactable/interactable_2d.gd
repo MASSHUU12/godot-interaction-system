49f49a5a4dc672dc617cfd84f4e48aeed731bfe5
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-08-12 11:42:40 +0200

    Update docs for Interactable2D

--- addons/interaction_system/systems/interaction/interactable/interactable_2d.gd
+++ addons/interaction_system/systems/interaction/interactable/interactable_2d.gd
@@ -1,6 +1,7 @@
 @tool
 extends Area2D
 
+## Class used to create interactive objects in 2D space.
 class_name Interactable2D
 
 ## Emitted when an [Interactor2D] starts looking at object.
@@ -9,7 +10,7 @@ signal focused(interactor: Interactor2D)
 signal unfocused(interactor: Interactor2D)
 ## Emitted when an [Interactor2D] interacts with an object.
 signal interacted(interactor: Interactor2D)
-## Emitted when an [Interactable] is the closest to the [Interactor2D].
+## Emitted when an [Interactable2D] is the closest to the [Interactor2D].
 signal closest(interactor: Interactor2D)
-## Emitted when an [Interactable] is no longer the closest one to the [Interactor2D].
+## Emitted when an [Interactable2D] is no longer the closest one to the [Interactor2D].
 signal not_closest(interactor: Interactor2D)
