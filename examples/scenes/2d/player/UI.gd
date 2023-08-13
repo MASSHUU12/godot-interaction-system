7e121bd4880d6f6e1abf928359750017b51076f7
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-08-13 12:55:04 +0200

    Update 2D example

---
+++ examples/scenes/2d/player/UI.gd
@@ -0,0 +1,45 @@
+extends Control
+
+@onready var timer: Timer = $Timer
+@onready var focus: Label = $VBoxContainer/Focus
+@onready var closest: Label = $VBoxContainer/Closest
+@onready var interact: Label = $VBoxContainer/Interact
+
+
+func _on_character_body_2d_interactor_closest_interactable(interactable: Interactable2D) -> void:
+	change_closest_text(interactable)
+
+
+func _on_character_body_2d_interactor_focused_on_interactable(interactable: Interactable2D) -> void:
+	change_focus_text(interactable)
+
+
+func _on_character_body_2d_interactor_not_closest_interactable(_interactable: Interactable2D) -> void:
+	change_closest_text(null)
+
+
+func _on_character_body_2d_interactor_unfocused_interactable(_interactable: Interactable2D) -> void:
+	change_focus_text(null)
+
+
+func _on_character_body_2d_interactor_interacted_with_interactable(interactable: Interactable2D) -> void:
+	timer.stop()
+	change_interact_text(interactable)
+	timer.start()
+
+
+
+func _on_timer_timeout() -> void:
+	change_interact_text(null)
+
+
+func change_focus_text(what: Interactable2D) -> void:
+	focus.text = "Focused on %s." % what
+
+
+func change_closest_text(what: Interactable2D) -> void:
+	closest.text = "Closest to %s." % what
+
+
+func change_interact_text(what: Interactable2D) -> void:
+	interact.text = "Interacted with %s." % what
