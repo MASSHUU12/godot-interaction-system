246253a2d81282985d677d2245756a27d7e8218f
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-09-29 14:09:10 +0200

    Add Interactable2D to the plugin

--- addons/interaction_system/InteractionSystem.cs
+++ addons/interaction_system/InteractionSystem.cs
@@ -9,11 +9,15 @@ public partial class InteractionSystem : EditorPlugin
 		var texture = GD.Load<Texture2D>("res://icon.png");
 		var script = GD.Load<Script>("res://addons/interaction_system/systems/interaction/interactable/Interactable3D.cs");
 		AddCustomType("Interactable3D", "Area3D", script, texture);
+
+		script = GD.Load<Script>("res://addons/interaction_system/systems/interaction/interactable/Interactable2D.cs");
+		AddCustomType("Interactable2D", "Area2D", script, texture);
 	}
 
 	public override void _ExitTree()
 	{
 		RemoveCustomType("Interactable3D");
+		RemoveCustomType("Interactable2D");
 	}
 }
 #endif
