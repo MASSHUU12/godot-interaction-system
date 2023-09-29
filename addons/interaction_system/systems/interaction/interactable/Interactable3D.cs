2583a4bf17d5aff0e40c50ee67334abdbdcffcac
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-09-29 15:04:54 +0200

    Use Interactor3D in signal's arguments

--- addons/interaction_system/systems/interaction/interactable/Interactable3D.cs
+++ addons/interaction_system/systems/interaction/interactable/Interactable3D.cs
@@ -2,9 +2,9 @@ using Godot;
 
 public partial class Interactable3D : Area3D, IInteractable
 {
-	[Signal] public delegate void InteractedEventHandler();
-	[Signal] public delegate void ClosestEventHandler();
-	[Signal] public delegate void NotClosestEventHandler();
-	[Signal] public delegate void FocusedEventHandler();
-	[Signal] public delegate void UnfocusedEventHandler();
+	[Signal] public delegate void InteractedEventHandler(Interactor3D interactor);
+	[Signal] public delegate void ClosestEventHandler(Interactor3D interactor);
+	[Signal] public delegate void NotClosestEventHandler(Interactor3D interactor);
+	[Signal] public delegate void FocusedEventHandler(Interactor3D interactor);
+	[Signal] public delegate void UnfocusedEventHandler(Interactor3D interactor);
 }
