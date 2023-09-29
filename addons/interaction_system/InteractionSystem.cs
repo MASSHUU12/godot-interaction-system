a0d0fb74a7e578b007fe16cc29153a1e6fc295db
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-09-29 13:33:21 +0200

    Change main file to C#

---
+++ addons/interaction_system/InteractionSystem.cs
@@ -0,0 +1,42 @@
+// @tool
+// extends EditorPlugin
+
+
+// func _enter_tree() -> void:
+// 	add_custom_type("Interactor3D", "Node3D", preload("./systems/interaction/interactor/interactor_3d.gd"), preload("res://icon.png"))
+// 	add_custom_type("Interactor2D", "Node2D", preload("./systems/interaction/interactor/interactor_2d.gd"), preload("res://icon.png"))
+// 	add_custom_type("CharacterBody3DInteractor", "Interactor3D", preload("./systems/interaction/interactor/character_body_3d_interactor.gd"), preload("res://icon.png"))
+// 	add_custom_type("CharacterBody2DInteractor", "Interactor2D", preload("./systems/interaction/interactor/character_body_2d_interactor.gd"), preload("res://icon.png"))
+
+// 	add_custom_type("Interactable3D", "Area3D", preload("./systems/interaction/interactable/interactable_3d.gd"), preload("res://icon.png"))
+// 	add_custom_type("Interactable2D", "Area2D", preload("./systems/interaction/interactable/interactable_2d.gd"), preload("res://icon.png"))
+// 	add_custom_type("InteractableProp", "InteractableProp", preload("./systems/interaction/interactable/interactable_prop.gd"), preload("res://icon.png"))
+
+
+// func _exit_tree() -> void:
+// 	remove_custom_type("Interactor2D")
+// 	remove_custom_type("Interactor3D")
+// 	remove_custom_type("CharacterBody2DInteractor")
+// 	remove_custom_type("CharacterBody3DInteractor")
+
+// 	remove_custom_type("Interactable2D")
+// 	remove_custom_type("Interactable3D")
+// 	remove_custom_type("InteractableProp")
+
+#if TOOLS
+using Godot;
+
+[Tool]
+public partial class CustomNode : EditorPlugin
+{
+    public override void _EnterTree()
+    {
+        // Initialization of the plugin goes here.
+    }
+
+    public override void _ExitTree()
+    {
+        // Clean-up of the plugin goes here.
+    }
+}
+#endif
