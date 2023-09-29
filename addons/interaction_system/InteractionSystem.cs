4533b3abca5c4617bbb3e9c0b2123e8e46e8fb9d
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-09-29 17:02:39 +0200

    Add Interactor2D to the plugin

--- addons/interaction_system/InteractionSystem.cs
+++ addons/interaction_system/InteractionSystem.cs
@@ -15,6 +15,9 @@ public partial class InteractionSystem : EditorPlugin
 
 		script = GD.Load<Script>("res://addons/interaction_system/systems/interaction/interactor/Interactor3D.cs");
 		AddCustomType("Interactor3D", "Node3D", script, texture);
+
+		script = GD.Load<Script>("res://addons/interaction_system/systems/interaction/interactor/Interactor2D.cs");
+		AddCustomType("Interactor2D", "Node2D", script, texture);
 	}
 
 	public override void _ExitTree()
@@ -23,6 +26,7 @@ public partial class InteractionSystem : EditorPlugin
 		RemoveCustomType("Interactable2D");
 
 		RemoveCustomType("Interactor3D");
+		RemoveCustomType("Interactor2D");
 	}
 }
 #endif
