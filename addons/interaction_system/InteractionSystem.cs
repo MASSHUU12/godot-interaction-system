3ecc1cb57d4f5cb0f74fad90d903fba646ae270f
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-09-29 14:05:53 +0200

    Add Interactable3D to the plugin

--- addons/interaction_system/InteractionSystem.cs
+++ addons/interaction_system/InteractionSystem.cs
@@ -1,42 +1,19 @@
-// @tool
-// extends EditorPlugin
-
-
-// func _enter_tree() -> void:
-// 	add_custom_type("Interactor3D", "Node3D", preload("./systems/interaction/interactor/interactor_3d.gd"), preload("res://icon.png"))
-// 	add_custom_type("Interactor2D", "Node2D", preload("./systems/interaction/interactor/interactor_2d.gd"), preload("res://icon.png"))
-// 	add_custom_type("CharacterBody3DInteractor", "Interactor3D", preload("./systems/interaction/interactor/character_body_3d_interactor.gd"), preload("res://icon.png"))
-// 	add_custom_type("CharacterBody2DInteractor", "Interactor2D", preload("./systems/interaction/interactor/character_body_2d_interactor.gd"), preload("res://icon.png"))
-
-// 	add_custom_type("Interactable3D", "Area3D", preload("./systems/interaction/interactable/interactable_3d.gd"), preload("res://icon.png"))
-// 	add_custom_type("Interactable2D", "Area2D", preload("./systems/interaction/interactable/interactable_2d.gd"), preload("res://icon.png"))
-// 	add_custom_type("InteractableProp", "InteractableProp", preload("./systems/interaction/interactable/interactable_prop.gd"), preload("res://icon.png"))
-
-
-// func _exit_tree() -> void:
-// 	remove_custom_type("Interactor2D")
-// 	remove_custom_type("Interactor3D")
-// 	remove_custom_type("CharacterBody2DInteractor")
-// 	remove_custom_type("CharacterBody3DInteractor")
-
-// 	remove_custom_type("Interactable2D")
-// 	remove_custom_type("Interactable3D")
-// 	remove_custom_type("InteractableProp")
-
 #if TOOLS
 using Godot;
 
 [Tool]
-public partial class CustomNode : EditorPlugin
+public partial class InteractionSystem : EditorPlugin
 {
-    public override void _EnterTree()
-    {
-        // Initialization of the plugin goes here.
-    }
+	public override void _EnterTree()
+	{
+		var texture = GD.Load<Texture2D>("res://icon.png");
+		var script = GD.Load<Script>("res://addons/interaction_system/systems/interaction/interactable/Interactable3D.cs");
+		AddCustomType("Interactable3D", "Area3D", script, texture);
+	}
 
-    public override void _ExitTree()
-    {
-        // Clean-up of the plugin goes here.
-    }
+	public override void _ExitTree()
+	{
+		RemoveCustomType("Interactable3D");
+	}
 }
 #endif
