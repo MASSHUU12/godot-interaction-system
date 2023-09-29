211717d8796de42ce33ea873638ed5521fd7aeda
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-09-29 14:41:50 +0200

    Add IInteractor as argument for signals

--- addons/interaction_system/systems/interaction/interactable/Interactable.cs
+++ addons/interaction_system/systems/interaction/interactable/Interactable.cs
@@ -2,9 +2,9 @@ using Godot;
 
 public partial interface IInteractable
 {
-	[Signal] public delegate void InteractedEventHandler();
-	[Signal] public delegate void ClosestEventHandler();
-	[Signal] public delegate void NotClosestEventHandler();
-	[Signal] public delegate void FocusedEventHandler();
-	[Signal] public delegate void UnfocusedEventHandler();
+	[Signal] public delegate void InteractedEventHandler(IInteractor interactor);
+	[Signal] public delegate void ClosestEventHandler(IInteractor interactor);
+	[Signal] public delegate void NotClosestEventHandler(IInteractor interactor);
+	[Signal] public delegate void FocusedEventHandler(IInteractor interactor);
+	[Signal] public delegate void UnfocusedEventHandler(IInteractor interactor);
 }
