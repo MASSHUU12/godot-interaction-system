64db666dfb8ef9f9ca5234078e353bc3e0263907
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-09-30 13:09:23 +0200

    Use Label3D to display interaction state

--- scenes/interactables/3D/interactable_box/InteractableBox.cs
+++ scenes/interactables/3D/interactable_box/InteractableBox.cs
@@ -2,10 +2,12 @@ using Godot;
 
 public partial class InteractableBox : Node3D
 {
+	private Label3D _label;
 	private Interactable3D _interactable;
 
 	public override void _Ready()
 	{
+		_label = GetNode<Label3D>("Label3D");
 		_interactable = GetNode<Interactable3D>("Interactable3D");
 		_interactable.Interacted += OnInteract;
 		_interactable.Closest += OnClosest;
@@ -16,26 +18,26 @@ public partial class InteractableBox : Node3D
 
 	private void OnInteract(Interactor3D interactor)
 	{
-		GD.Print("Interacted with the box!");
+		_label.Text = "Interacted with the box!";
 	}
 
 	private void OnClosest(Interactor3D interactor)
 	{
-		GD.Print("Closest to the box!");
+		_label.Text = "Closest to the box!";
 	}
 
 	private void OnNotClosest(Interactor3D interactor)
 	{
-		GD.Print("Not closest to the box!");
+		_label.Text = "Not closest to the box!";
 	}
 
 	private void OnFocused(Interactor3D interactor)
 	{
-		GD.Print("Focused on the box!");
+		_label.Text = "Focused on the box!";
 	}
 
 	private void OnUnfocused(Interactor3D interactor)
 	{
-		GD.Print("Unfocused from the box!");
+		_label.Text = "Unfocused from the box!";
 	}
 }
