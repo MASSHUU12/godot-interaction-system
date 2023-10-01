d38548deec466d77aa202236e5697feb0e861872
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-10-01 15:27:00 +0200

    Reset interaction label after 1s from interaction

--- scenes/player/UI/PlayerUI.gd
+++ scenes/player/UI/PlayerUI.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var timer: Timer = $Timer
 @onready var character_interactor_3d: Node3D = $"../CharacterInteractor3D"
 
 @onready var interaction_label: Label = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/InteractionLabel
@@ -8,6 +9,7 @@ extends Control
 
 
 func _ready() -> void:
+	timer.connect("timeout", _on_timer_timeout)
 	character_interactor_3d.connect("UnfocusedInteractable", _on_character_interactor_3d_unfocused_interactable)
 	character_interactor_3d.connect("ClosestToInteractable", _on_character_interactor_3d_closest_to_interactable)
 	character_interactor_3d.connect("FocusedOnInteractable", _on_character_interactor_3d_focused_on_interactable)
@@ -17,6 +19,7 @@ func _ready() -> void:
 
 func _on_character_interactor_3d_interacted_with_interactable(interactable: Node3D) -> void:
 	interaction_label.text = "Interacting with: " + str(interactable)
+	timer.start()
 
 
 func _on_character_interactor_3d_focused_on_interactable(interactable: Node3D) -> void:
@@ -33,3 +36,7 @@ func _on_character_interactor_3d_closest_to_interactable(interactable: Node3D) -
 
 func _on_character_interactor_3d_not_closest_to_interactable(_interactable: Node3D) -> void:
 	closest_label.text = "Closest to: none"
+
+
+func _on_timer_timeout() -> void:
+	interaction_label.text = "Interacting with: none"
