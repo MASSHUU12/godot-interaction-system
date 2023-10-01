5645496f3fbf900c3a246966f1b205c74d20ac41
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-10-01 15:24:30 +0200

    Use labels instead of print to display events

--- scenes/player/UI/PlayerUI.gd
+++ scenes/player/UI/PlayerUI.gd
@@ -2,6 +2,10 @@ extends Control
 
 @onready var character_interactor_3d: Node3D = $"../CharacterInteractor3D"
 
+@onready var interaction_label: Label = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/InteractionLabel
+@onready var focus_label: Label = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/FocusLabel
+@onready var closest_label: Label = $MarginContainer/PanelContainer/MarginContainer/VBoxContainer/ClosestLabel
+
 
 func _ready() -> void:
 	character_interactor_3d.connect("UnfocusedInteractable", _on_character_interactor_3d_unfocused_interactable)
@@ -11,21 +15,21 @@ func _ready() -> void:
 	character_interactor_3d.connect("InteractedWithInteractable", _on_character_interactor_3d_interacted_with_interactable)
 
 
-func _on_character_interactor_3d_closest_to_interactable(_interactable: Node3D) -> void:
-	print("closest")
+func _on_character_interactor_3d_interacted_with_interactable(interactable: Node3D) -> void:
+	interaction_label.text = "Interacting with: " + str(interactable)
 
 
-func _on_character_interactor_3d_focused_on_interactable(_interactable: Node3D) -> void:
-	print("focused")
-
-
-func _on_character_interactor_3d_interacted_with_interactable(_interactable: Node3D) -> void:
-	print("interacted")
-
-
-func _on_character_interactor_3d_not_closest_to_interactable(_interactable: Node3D) -> void:
-	print("not closest")
+func _on_character_interactor_3d_focused_on_interactable(interactable: Node3D) -> void:
+	focus_label.text = "Focusing on: " + str(interactable)
 
 
 func _on_character_interactor_3d_unfocused_interactable(_interactable: Node3D) -> void:
-	print("unfocused")
+	focus_label.text = "Focusing on: none"
+
+
+func _on_character_interactor_3d_closest_to_interactable(interactable: Node3D) -> void:
+	closest_label.text = "Closest to: " + str(interactable)
+
+
+func _on_character_interactor_3d_not_closest_to_interactable(_interactable: Node3D) -> void:
+	closest_label.text = "Closest to: none"
