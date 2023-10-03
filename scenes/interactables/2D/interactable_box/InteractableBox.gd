64d9c34cef919e764555cc39ee6460bc26e16992
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-10-03 10:31:00 +0200

    Add interaction logic to the interactable box

---
+++ scenes/interactables/2D/interactable_box/InteractableBox.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+
+@onready var label: Label = $Label
+@onready var interactable_2d: Area2D = $Interactable2D
+
+
+func _ready() -> void:
+	interactable_2d.connect("Interacted", _on_interactable_2d_interacted)
+	interactable_2d.connect("Focused", _on_interactable_2d_focused)
+	interactable_2d.connect("Unfocused", _on_interactable_2d_unfocused)
+	interactable_2d.connect("Closest", _on_interactable_2d_closest)
+	interactable_2d.connect("NotClosest", _on_interactable_2d_not_closest)
+
+
+func _on_interactable_2d_interacted(_interactor: Node2D) -> void:
+	label.text = "Interacted with the box!"
+
+
+func _on_interactable_2d_closest(_interactor: Node2D) -> void:
+	label.text = "Closest to the box!"
+
+
+func _on_interactable_2d_not_closest(_interactor: Node2D) -> void:
+	label.text = "Not closest to the box!"
+
+
+func _on_interactable_2d_focused(_interactor: Node2D) -> void:
+	label.text = "Focused on the box!"
+
+
+func _on_interactable_2d_unfocused(_interactor: Node2D) -> void:
+	label.text = "Unfocused from the box!"
