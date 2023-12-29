b0ed9d3c5baa4187adff83222d226a17d3f720ce
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-12-29 15:28:12 +0100

    Remove comments

--- addons/interaction_system/src/interactor/Interactor.cs
+++ addons/interaction_system/src/interactor/Interactor.cs
@@ -1,126 +1,98 @@
 using Godot;
+using InteractionSystem.Interactable;
 
 namespace InteractionSystem.Interactor
 {
 	public partial interface IInteractor
 	{
 		[Signal]
-		public delegate void InteractedWithInteractableEventHandler(Interactable.IInteractable interactable);
+		public delegate void InteractedWithInteractableEventHandler(IInteractable interactable);
 		[Signal]
-		public delegate void ClosestToInteractableEventHandler(Interactable.IInteractable interactable);
+		public delegate void ClosestToInteractableEventHandler(IInteractable interactable);
 		[Signal]
-		public delegate void NotClosestToInteractableEventHandler(Interactable.IInteractable interactable);
+		public delegate void NotClosestToInteractableEventHandler(IInteractable interactable);
 		[Signal]
-		public delegate void FocusedOnInteractableEventHandler(Interactable.IInteractable interactable);
+		public delegate void FocusedOnInteractableEventHandler(IInteractable interactable);
 		[Signal]
-		public delegate void UnfocusedInteractableEventHandler(Interactable.IInteractable interactable);
+		public delegate void UnfocusedInteractableEventHandler(IInteractable interactable);
 
-		/// <summary>
-		/// Interacts with the specified interactable.
-		/// </summary>
-		/// <param name="interactable">The interactable to interact with.</param>
-		protected void Interact(Interactable.IInteractable interactable);
-		/// <summary>
-		/// Focuses on the specified interactable object.
-		/// </summary>
-		/// <param name="interactable">The interactable object to focus on.</param>
-		protected void Focus(Interactable.IInteractable interactable);
-		/// <summary>
-		/// Unfocuses the specified interactable.
-		/// </summary>
-		/// <param name="interactable">The interactable to unfocus.</param>
-		protected void Unfocus(Interactable.IInteractable interactable);
-		/// <summary>
-		/// Finds the closest interactable object to the interactor and interacts with it.
-		/// </summary>
-		/// <param name="interactable">The closest interactable object to the interactor.</param>
-		protected void Closest(Interactable.IInteractable interactable);
-		/// <summary>
-		/// Called when the interactor is not the closest to an interactable object.
-		/// </summary>
-		/// <param name="interactable">The interactable object that is not the closest to the interactor.</param>
-		protected void NotClosest(Interactable.IInteractable interactable);
+		protected void Interact(IInteractable interactable);
+		protected void Focus(IInteractable interactable);
+		protected void Unfocus(IInteractable interactable);
+		protected void Closest(IInteractable interactable);
+		protected void NotClosest(IInteractable interactable);
 
-		/// <summary>
-		/// Returns the closest interactable object within the interactor's range.
-		/// </summary>
-		/// <returns>The closest interactable object within range, or null if none are found.</returns>
-		protected Interactable.IInteractable GetClosestInteractable();
-		/// <summary>
-		/// Returns the interactable object that is currently being raycasted by the interactor.
-		/// </summary>
-		/// <returns>The interactable object that is currently being raycasted by the interactor,
-		/// or null if no interactable object is found.</returns>
-		protected Interactable.IInteractable GetRayCastedInteractable();
+		protected IInteractable GetClosestInteractable();
+		protected IInteractable GetRayCastedInteractable();
 	}
 
 	static partial class Interactor
 	{
-		public static void Interact(Interactable.Interactable3D interactable, Interactor3D interactor)
+		public static void Interact(Interactable3D interactable, Interactor3D interactor)
 		{
 			interactable.EmitSignal(nameof(interactable.Interacted), interactor);
 			interactor.EmitSignal(nameof(interactor.InteractedWithInteractable), interactable);
 		}
 
-		public static void Interact(Interactable.Interactable2D interactable, Interactor2D interactor)
+		public static void Interact(Interactable2D interactable, Interactor2D interactor)
 		{
 			interactable.EmitSignal(nameof(interactable.Interacted), interactor);
 			interactor.EmitSignal(nameof(interactor.InteractedWithInteractable), interactable);
 		}
 
-		public static void Focus(Interactable.Interactable3D interactable, Interactor3D interactor)
+		public static void Focus(Interactable3D interactable, Interactor3D interactor)
 		{
 			interactable.EmitSignal(nameof(interactable.Focused), interactor);
 			interactor.EmitSignal(nameof(interactor.FocusedOnInteractable), interactable);
 		}
 
-		public static void Focus(Interactable.Interactable2D interactable, Interactor2D interactor)
+		public static void Focus(Interactable2D interactable, Interactor2D interactor)
 		{
 			interactable.EmitSignal(nameof(interactable.Focused), interactor);
 			interactor.EmitSignal(nameof(interactor.FocusedOnInteractable), interactable);
 		}
 
-		public static void Unfocus(Interactable.Interactable3D interactable, Interactor3D interactor)
+		public static void Unfocus(Interactable3D interactable, Interactor3D interactor)
 		{
 			interactable.EmitSignal(nameof(interactable.Unfocused), interactor);
 			interactor.EmitSignal(nameof(interactor.UnfocusedInteractable), interactable);
 		}
 
-		public static void Unfocus(Interactable.Interactable2D interactable, Interactor2D interactor)
+		public static void Unfocus(Interactable2D interactable, Interactor2D interactor)
 		{
 			interactable.EmitSignal(nameof(interactable.Unfocused), interactor);
 			interactor.EmitSignal(nameof(interactor.UnfocusedInteractable), interactable);
 		}
 
-		public static void Closest(Interactable.Interactable3D interactable, Interactor3D interactor)
+		public static void Closest(Interactable3D interactable, Interactor3D interactor)
 		{
 			interactable.EmitSignal(nameof(interactable.Closest), interactor);
 			interactor.EmitSignal(nameof(interactor.ClosestToInteractable), interactable);
 		}
 
-		public static void Closest(Interactable.Interactable2D interactable, Interactor2D interactor)
+		public static void Closest(Interactable2D interactable, Interactor2D interactor)
 		{
 			interactable.EmitSignal(nameof(interactable.Closest), interactor);
 			interactor.EmitSignal(nameof(interactor.ClosestToInteractable), interactable);
 		}
 
-		public static void NotClosest(Interactable.Interactable3D interactable, Interactor3D interactor)
+		public static void NotClosest(Interactable3D interactable, Interactor3D interactor)
 		{
 			interactable.EmitSignal(nameof(interactable.NotClosest), interactor);
 			interactor.EmitSignal(nameof(interactor.NotClosestToInteractable), interactable);
 		}
 
-		public static void NotClosest(Interactable.Interactable2D interactable, Interactor2D interactor)
+		public static void NotClosest(Interactable2D interactable, Interactor2D interactor)
 		{
 			interactable.EmitSignal(nameof(interactable.NotClosest), interactor);
 			interactor.EmitSignal(nameof(interactor.NotClosestToInteractable), interactable);
 		}
 
-		public static Interactable.IInteractable GetRayCastedInteractable(
+		public static IInteractable GetRayCastedInteractable(
 			RayCast3D rayCast3D = null, RayCast2D rayCast2D = null
 		)
 		{
-			return (Interactable.IInteractable)(rayCast3D?.GetCollider() ?? rayCast2D?.GetCollider()) ?? null;
+			return (IInteractable)(rayCast3D?.GetCollider() ?? rayCast2D?.GetCollider()) ?? null;
 		}
 	}
 }
