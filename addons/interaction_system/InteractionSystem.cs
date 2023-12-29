5f7d03b2b923407a30c91f14ba8ea02006c126c2
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-12-29 12:38:12 +0100

    Update project folder structure

--- addons/interaction_system/InteractionSystem.cs
+++ addons/interaction_system/InteractionSystem.cs
@@ -7,25 +7,25 @@ public partial class InteractionSystem : EditorPlugin
 	public override void _EnterTree()
 	{
 		var texture = GD.Load<Texture2D>("res://icon.png");
-		var script = GD.Load<Script>("res://addons/interaction_system/systems/interaction/interactable/Interactable3D.cs");
+		var script = GD.Load<Script>("res://addons/interaction_system/src/interactable/Interactable3D.cs");
 		AddCustomType("Interactable3D", "Area3D", script, texture);
 
-		script = GD.Load<Script>("res://addons/interaction_system/systems/interaction/interactable/InteractableProp3D.cs");
+		script = GD.Load<Script>("res://addons/interaction_system/src/interactable/InteractableProp3D.cs");
 		AddCustomType("InteractableProp3D", "Area3D", script, texture);
 
-		script = GD.Load<Script>("res://addons/interaction_system/systems/interaction/interactable/Interactable2D.cs");
+		script = GD.Load<Script>("res://addons/interaction_system/src/interactable/Interactable2D.cs");
 		AddCustomType("Interactable2D", "Area2D", script, texture);
 
-		script = GD.Load<Script>("res://addons/interaction_system/systems/interaction/interactor/Interactor3D.cs");
+		script = GD.Load<Script>("res://addons/interaction_system/src/interactor/Interactor3D.cs");
 		AddCustomType("Interactor3D", "Node3D", script, texture);
 
-		script = GD.Load<Script>("res://addons/interaction_system/systems/interaction/interactor/Interactor2D.cs");
+		script = GD.Load<Script>("res://addons/interaction_system/src/interactor/Interactor2D.cs");
 		AddCustomType("Interactor2D", "Node2D", script, texture);
 
-		script = GD.Load<Script>("res://addons/interaction_system/systems/interaction/interactor/CharacterInteractor3D.cs");
+		script = GD.Load<Script>("res://addons/interaction_system/src/interactor/CharacterInteractor3D.cs");
 		AddCustomType("CharacterInteractor3D", "Node3D", script, texture);
 
-		script = GD.Load<Script>("res://addons/interaction_system/systems/interaction/interactor/CharacterInteractor2D.cs");
+		script = GD.Load<Script>("res://addons/interaction_system/src/interactor/CharacterInteractor2D.cs");
 		AddCustomType("CharacterInteractor2D", "Node2D", script, texture);
 	}
 
