aec36e6d97462daa5b6c6649530afb9c27e7cfd4
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-12-29 13:38:21 +0100

    Cleanup custom type registration

--- addons/interaction_system/InteractionSystem.cs
+++ addons/interaction_system/InteractionSystem.cs
@@ -6,42 +6,34 @@ namespace InteractionSystem
 	[Tool]
 	public partial class InteractionSystem : EditorPlugin
 	{
+		private const string commonPath = "res://addons/interaction_system/src/";
+
 		public override void _EnterTree()
 		{
 			var texture = GD.Load<Texture2D>("res://icon.png");
-			var script = GD.Load<Script>("res://addons/interaction_system/src/interactable/Interactable3D.cs");
-			AddCustomType("Interactable3D", "Area3D", script, texture);
 
-			script = GD.Load<Script>("res://addons/interaction_system/src/interactable/InteractableProp3D.cs");
-			AddCustomType("InteractableProp3D", "Area3D", script, texture);
+			AddCustomType("Interactable2D", "Area2D", GD.Load<Script>(commonPath + "interactable/Interactable2D.cs"), texture);
+			AddCustomType("Interactable3D", "Area3D", GD.Load<Script>(commonPath + "interactable/Interactable3D.cs"), texture);
+			AddCustomType("InteractableProp3D", "Area3D", GD.Load<Script>(commonPath + "interactable/InteractableProp3D.cs"), texture);
 
-			script = GD.Load<Script>("res://addons/interaction_system/src/interactable/Interactable2D.cs");
-			AddCustomType("Interactable2D", "Area2D", script, texture);
+			AddCustomType("Interactor2D", "Node2D", GD.Load<Script>(commonPath + "interactor/Interactor2D.cs"), texture);
+			AddCustomType("Interactor3D", "Node3D", GD.Load<Script>(commonPath + "interactor/Interactor3D.cs"), texture);
 
-			script = GD.Load<Script>("res://addons/interaction_system/src/interactor/Interactor3D.cs");
-			AddCustomType("Interactor3D", "Node3D", script, texture);
-
-			script = GD.Load<Script>("res://addons/interaction_system/src/interactor/Interactor2D.cs");
-			AddCustomType("Interactor2D", "Node2D", script, texture);
-
-			script = GD.Load<Script>("res://addons/interaction_system/src/interactor/character_interactor/CharacterInteractor3D.cs");
-			AddCustomType("CharacterInteractor3D", "Node3D", script, texture);
-
-			script = GD.Load<Script>("res://addons/interaction_system/src/interactor/character_interactor/CharacterInteractor2D.cs");
-			AddCustomType("CharacterInteractor2D", "Node2D", script, texture);
+			AddCustomType("CharacterInteractor2D", "Node2D", GD.Load<Script>(commonPath + "interactor/character_interactor/CharacterInteractor2D.cs"), texture);
+			AddCustomType("CharacterInteractor3D", "Node3D", GD.Load<Script>(commonPath + "interactor/character_interactor/CharacterInteractor3D.cs"), texture);
 		}
 
 		public override void _ExitTree()
 		{
+			RemoveCustomType("Interactable2D");
 			RemoveCustomType("Interactable3D");
 			RemoveCustomType("InteractableProp3D");
-			RemoveCustomType("Interactable2D");
 
-			RemoveCustomType("Interactor3D");
 			RemoveCustomType("Interactor2D");
+			RemoveCustomType("Interactor3D");
 
-			RemoveCustomType("CharacterInteractor3D");
 			RemoveCustomType("CharacterInteractor2D");
+			RemoveCustomType("CharacterInteractor3D");
 		}
 	}
 }
