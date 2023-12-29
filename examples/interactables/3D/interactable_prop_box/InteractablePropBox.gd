dead37aa1287a4a80cb04ba928035092a2e7ccce
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-12-29 18:40:04 +0100

    Fix signal usage

--- examples/interactables/3D/interactable_prop_box/InteractablePropBox.gd
+++ examples/interactables/3D/interactable_prop_box/InteractablePropBox.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 @onready var label_3d: Label3D = $Label3D
-@onready var interactable_prop_3d: Area3D = $InteractableProp3D
+@onready var interactable_prop_3d: Node = $InteractableProp3D
 
 
 func _ready() -> void:
@@ -12,21 +12,21 @@ func _ready() -> void:
 	interactable_prop_3d.connect("NotClosest", _on_interactable_prop_3d_not_closest)
 
 
-func _on_interactable_prop_3d_interacted(_interactor: Node3D) -> void:
+func _on_interactable_prop_3d_interacted(_interactor: Node) -> void:
 	label_3d.text = "Interacted with the box!"
 
 
-func _on_interactable_prop_3d_closest(_interactor: Node3D) -> void:
+func _on_interactable_prop_3d_closest(_interactor: Node) -> void:
 	label_3d.text = "Closest to the box!"
 
 
-func _on_interactable_prop_3d_not_closest(_interactor: Node3D) -> void:
+func _on_interactable_prop_3d_not_closest(_interactor: Node) -> void:
 	label_3d.text = "Not closest to the box!"
 
 
-func _on_interactable_prop_3d_focused(_interactor: Node3D) -> void:
+func _on_interactable_prop_3d_focused(_interactor: Node) -> void:
 	label_3d.text = "Focused on the box!"
 
 
-func _on_interactable_prop_3d_unfocused(_interactor: Node3D) -> void:
+func _on_interactable_prop_3d_unfocused(_interactor: Node) -> void:
 	label_3d.text = "Unfocused from the box!"
