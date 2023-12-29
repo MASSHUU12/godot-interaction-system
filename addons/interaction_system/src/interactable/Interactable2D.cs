1a6ed2689687a86c28fc6336d346c01c16d1345f
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-12-29 17:01:15 +0100

    Add configuration warnings for Interactable2D/3D

--- addons/interaction_system/src/interactable/Interactable2D.cs
+++ addons/interaction_system/src/interactable/Interactable2D.cs
@@ -1,9 +1,48 @@
+using System.Collections.Generic;
 using Godot;
 
 namespace InteractionSystem.Interactable
 {
+	[Tool]
 	public partial class Interactable2D : Interactable
 	{
-		[Export] public Area2D Area { get; set; }
+		[Export]
+		public Area2D Area
+		{
+			get => _area;
+			set
+			{
+				if (value != _area)
+				{
+					_area = value;
+					UpdateConfigurationWarnings();
+				}
+			}
+		}
+
+		protected Area2D _area;
+
+		public override string[] _GetConfigurationWarnings()
+		{
+			List<string> warnings = new();
+
+			if (_area == null)
+			{
+				var warning = "This node does not have the ability to be interacted with. " +
+					"Please add an Area2D to this node.";
+				warnings.Add(warning);
+			}
+
+			if (_area.GetParent() != this)
+			{
+				var warning = "The Area2D is not a child of this node. " +
+					"Please add the Area2D as a child of this node.";
+				warnings.Add(warning);
+			}
+
+			warnings.AddRange(base._GetConfigurationWarnings() ?? System.Array.Empty<string>());
+
+			return warnings.ToArray();
+		}
 	}
 }
--- addons/interaction_system/src/interactable/Interactable3D.cs
+++ addons/interaction_system/src/interactable/Interactable3D.cs
@@ -1,9 +1,48 @@
+using System.Collections.Generic;
 using Godot;
 
 namespace InteractionSystem.Interactable
 {
+	[Tool]
 	public partial class Interactable3D : Interactable
 	{
-		[Export] public Area3D Area { get; set; }
+		[Export]
+		public Area3D Area
+		{
+			get => _area;
+			set
+			{
+				if (value != _area)
+				{
+					_area = value;
+					UpdateConfigurationWarnings();
+				}
+			}
+		}
+
+		protected Area3D _area;
+
+		public override string[] _GetConfigurationWarnings()
+		{
+			List<string> warnings = new();
+
+			if (_area is null)
+			{
+				var warning = "This node does not have the ability to be interacted with. " +
+					"Please add an Area3D to this node.";
+				warnings.Add(warning);
+			}
+
+			if (_area.GetParent() != this)
+			{
+				var warning = "The Area3D is not a child of this node. " +
+					"Please add the Area3D as a child of this node.";
+				warnings.Add(warning);
+			}
+
+			warnings.AddRange(base._GetConfigurationWarnings() ?? System.Array.Empty<string>());
+
+			return warnings.ToArray();
+		}
 	}
 }
