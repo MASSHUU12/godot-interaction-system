d7dfe0b923313163f8be7f09cf9c4e67116b049d
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-12-29 18:48:48 +0100

    Adjust scenes to the new system

--- examples/interactables/2D/interactable_box/InteractableBox.gd
+++ examples/interactables/2D/interactable_box/InteractableBox.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 @onready var label: Label = $Label
-@onready var interactable_2d: Area2D = $Interactable2D
+@onready var interactable_2d: Node = $Interactable2D
 
 
 func _ready() -> void:
@@ -13,21 +13,21 @@ func _ready() -> void:
 	interactable_2d.connect("NotClosest", _on_interactable_2d_not_closest)
 
 
-func _on_interactable_2d_interacted(_interactor: Node2D) -> void:
+func _on_interactable_2d_interacted(_interactor: Node) -> void:
 	label.text = "Interacted with the box!"
 
 
-func _on_interactable_2d_closest(_interactor: Node2D) -> void:
+func _on_interactable_2d_closest(_interactor: Node) -> void:
 	label.text = "Closest to the box!"
 
 
-func _on_interactable_2d_not_closest(_interactor: Node2D) -> void:
+func _on_interactable_2d_not_closest(_interactor: Node) -> void:
 	label.text = "Not closest to the box!"
 
 
-func _on_interactable_2d_focused(_interactor: Node2D) -> void:
+func _on_interactable_2d_focused(_interactor: Node) -> void:
 	label.text = "Focused on the box!"
 
 
-func _on_interactable_2d_unfocused(_interactor: Node2D) -> void:
+func _on_interactable_2d_unfocused(_interactor: Node) -> void:
 	label.text = "Unfocused from the box!"
