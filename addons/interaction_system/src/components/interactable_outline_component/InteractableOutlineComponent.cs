58254dc669619cf4cf0334ce38b0f9987de4a242
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-12-30 14:54:12 +0100

    Add support for 2D & 3D

--- addons/interaction_system/src/components/interactable_outline_component/InteractableOutlineComponent.cs
+++ addons/interaction_system/src/components/interactable_outline_component/InteractableOutlineComponent.cs
@@ -6,12 +6,15 @@ namespace InteractionSystem.Components
 	public partial class InteractableOutlineComponent : Node
 	{
 		[Export] public bool Enabled { get; set; } = true;
-		[Export] public MeshInstance3D Outline { get; set; }
+		[Export] public Node Outline { get; set; }
 		[Export] public Interactable.Interactable Prop { get; set; }
 
 		[ExportGroup("Outline On")]
 		[Export] public bool OutlineOnFocus { get; set; } = true;
 		[Export] public bool OutlineOnClosest { get; set; } = true;
+
+		private MeshInstance2D _outline2D;
+		private MeshInstance3D _outline3D;
 
 		public override void _Ready()
 		{
@@ -22,27 +25,47 @@ namespace InteractionSystem.Components
 			Prop.Closest += OnClosest;
 			Prop.NotClosest += OnNotClosest;
 
-			Outline.Hide();
+			InitializeOutline();
+
+			HideOutline();
+		}
+
+		private void InitializeOutline()
+		{
+			if (Outline is MeshInstance2D) _outline2D = Outline as MeshInstance2D;
+			else if (Outline is MeshInstance3D) _outline3D = Outline as MeshInstance3D;
+		}
+
+		private void ShowOutline()
+		{
+			_outline2D?.Show();
+			_outline3D?.Show();
+		}
+
+		private void HideOutline()
+		{
+			_outline2D?.Hide();
+			_outline3D?.Hide();
 		}
 
 		private void OnFocus(Interactor.Interactor interactor)
 		{
-			if (OutlineOnFocus) Outline.Show();
+			if (OutlineOnFocus) ShowOutline();
 		}
 
 		private void OnFocusLost(Interactor.Interactor interactor)
 		{
-			if (OutlineOnFocus) Outline.Hide();
+			if (OutlineOnFocus) HideOutline();
 		}
 
 		private void OnClosest(Interactor.Interactor interactor)
 		{
-			if (OutlineOnClosest) Outline.Show();
+			if (OutlineOnClosest) ShowOutline();
 		}
 
 		private void OnNotClosest(Interactor.Interactor interactor)
 		{
-			if (OutlineOnClosest) Outline.Hide();
+			if (OutlineOnClosest) HideOutline();
 		}
 	}
 }
