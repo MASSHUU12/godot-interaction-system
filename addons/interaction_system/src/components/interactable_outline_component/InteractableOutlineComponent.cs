d6e0308565863bb53156ee514a6cb3a1120f9be9
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-12-30 15:09:05 +0100

    Add configuration warnings

--- addons/interaction_system/src/components/interactable_outline_component/InteractableOutlineComponent.cs
+++ addons/interaction_system/src/components/interactable_outline_component/InteractableOutlineComponent.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Godot;
 
 namespace InteractionSystem.Components
@@ -6,13 +7,39 @@ namespace InteractionSystem.Components
 	public partial class InteractableOutlineComponent : Node
 	{
 		[Export] public bool Enabled { get; set; } = true;
-		[Export] public Node Outline { get; set; }
-		[Export] public Interactable.Interactable Prop { get; set; }
+		[Export]
+		public Node Outline
+		{
+			get => _outline;
+			set
+			{
+				if (value != _outline)
+				{
+					_outline = value;
+					UpdateConfigurationWarnings();
+				}
+			}
+		}
+		[Export]
+		public Interactable.Interactable Prop
+		{
+			get => _prop;
+			set
+			{
+				if (value != _prop)
+				{
+					_prop = value;
+					UpdateConfigurationWarnings();
+				}
+			}
+		}
 
 		[ExportGroup("Outline On")]
 		[Export] public bool OutlineOnFocus { get; set; } = true;
 		[Export] public bool OutlineOnClosest { get; set; } = true;
 
+		private Interactable.Interactable _prop;
+		private Node _outline;
 		private MeshInstance2D _outline2D;
 		private MeshInstance3D _outline3D;
 
@@ -28,6 +55,21 @@ namespace InteractionSystem.Components
 			InitializeOutline();
 
 			HideOutline();
+		}
+
+		public override string[] _GetConfigurationWarnings()
+		{
+			List<string> warnings = new();
+
+			if (Outline is null) warnings.Add("Outline is null");
+			else if (Outline is not MeshInstance2D && Outline is not MeshInstance3D)
+				warnings.Add("Outline is not a MeshInstance2D or MeshInstance3D");
+
+			if (Prop is null) warnings.Add("Prop is null");
+			else if (Prop is not Interactable.Interactable)
+				warnings.Add("Prop is not an Interactable");
+
+			return warnings.ToArray();
 		}
 
 		private void InitializeOutline()
