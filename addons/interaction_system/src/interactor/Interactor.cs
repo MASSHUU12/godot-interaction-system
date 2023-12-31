bc5830606ef50e53fa81f8080a8e7eaf3638562d
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-12-31 15:57:34 +0100

    Add properties for focus and closest interactable

--- addons/interaction_system/src/interactor/Interactor.cs
+++ addons/interaction_system/src/interactor/Interactor.cs
@@ -15,6 +15,12 @@ namespace InteractionSystem.Interactor
 		[Signal]
 		public delegate void UnfocusedInteractableEventHandler(Interactor interactable);
 
+		public bool IsFocused { get; private set; } = false;
+		public Interactable.Interactable Focusing { get; private set; } = null;
+
+		public bool IsClosest { get; private set; } = false;
+		public Interactable.Interactable ClosestInteractable { get; private set; } = null;
+
 		public void Interact(Interactable.Interactable interactable)
 		{
 			interactable.EmitSignal(nameof(interactable.Interacted), this);
@@ -23,24 +29,36 @@ namespace InteractionSystem.Interactor
 
 		public void Focus(Interactable.Interactable interactable)
 		{
+			IsFocused = true;
+			Focusing = interactable;
+
 			interactable.EmitSignal(nameof(interactable.Focused), this);
 			EmitSignal(SignalName.FocusedOnInteractable, interactable);
 		}
 
 		public void Unfocus(Interactable.Interactable interactable)
 		{
+			IsFocused = false;
+			Focusing = null;
+
 			interactable.EmitSignal(nameof(interactable.Unfocused), this);
 			EmitSignal(SignalName.UnfocusedInteractable, interactable);
 		}
 
 		public void Closest(Interactable.Interactable interactable)
 		{
+			IsClosest = true;
+			ClosestInteractable = interactable;
+
 			interactable.EmitSignal(nameof(interactable.Closest), this);
 			EmitSignal(SignalName.ClosestToInteractable, interactable);
 		}
 
 		public void NotClosest(Interactable.Interactable interactable)
 		{
+			IsClosest = false;
+			ClosestInteractable = null;
+
 			interactable.EmitSignal(nameof(interactable.NotClosest), this);
 			EmitSignal(SignalName.NotClosestToInteractable, interactable);
 		}
