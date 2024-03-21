3e75af92cb035a2f4207d845fc2dde047504a367
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-03-21 11:14:25 +0100

    Add LongInteracted signal

--- addons/interaction_system/src/interactable/Interactable.cs
+++ addons/interaction_system/src/interactable/Interactable.cs
@@ -1,13 +1,13 @@
 using Godot;
 
-namespace InteractionSystem.Interactable
+namespace InteractionSystem.Interactable;
+
+public partial class Interactable : Node
 {
-	public partial class Interactable : Node
-	{
-		[Signal] public delegate void InteractedEventHandler(Interactor.Interactor interactor);
-		[Signal] public delegate void ClosestEventHandler(Interactor.Interactor interactor);
-		[Signal] public delegate void NotClosestEventHandler(Interactor.Interactor interactor);
-		[Signal] public delegate void FocusedEventHandler(Interactor.Interactor interactor);
-		[Signal] public delegate void UnfocusedEventHandler(Interactor.Interactor interactor);
-	}
+	[Signal] public delegate void InteractedEventHandler(Interactor.Interactor interactor);
+	[Signal] public delegate void LongInteractedEventHandler(Interactor.Interactor interactor);
+	[Signal] public delegate void ClosestEventHandler(Interactor.Interactor interactor);
+	[Signal] public delegate void NotClosestEventHandler(Interactor.Interactor interactor);
+	[Signal] public delegate void FocusedEventHandler(Interactor.Interactor interactor);
+	[Signal] public delegate void UnfocusedEventHandler(Interactor.Interactor interactor);
 }
