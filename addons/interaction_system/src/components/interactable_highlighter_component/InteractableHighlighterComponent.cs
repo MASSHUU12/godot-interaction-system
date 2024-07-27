5c203001bd2374531e9a693dd08c604f7934415f
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-07-27 15:52:17 +0200

    style: add brackets to if statements

--- addons/interaction_system/src/components/interactable_highlighter_component/InteractableHighlighterComponent.cs
+++ addons/interaction_system/src/components/interactable_highlighter_component/InteractableHighlighterComponent.cs
@@ -60,7 +60,10 @@ public partial class InteractableHighlighterComponent : Node
 
     public override void _Ready()
     {
-        if (Engine.IsEditorHint()) return;
+        if (Engine.IsEditorHint())
+        {
+            return;
+        }
 
         Prop.Focused += OnFocus;
         Prop.Unfocused += OnFocusLost;
@@ -72,72 +75,122 @@ public partial class InteractableHighlighterComponent : Node
 
     public override void _Process(double delta)
     {
-        if (Engine.IsEditorHint()) return;
+        if (Engine.IsEditorHint())
+        {
+            return;
+        }
 
         if (!Enabled)
         {
             HideHighlighter();
             return;
         }
-        if (HighlightOn == EHighlightOn.Always) ShowHighlighter();
+        if (HighlightOn == EHighlightOn.Always)
+        {
+            ShowHighlighter();
+        }
     }
 
     public override string[] _GetConfigurationWarnings()
     {
         List<string> warnings = new();
 
-        if (Mesh is null) warnings.Add("Mesh is null");
-        else if (Mesh is not MeshInstance2D && Mesh is not MeshInstance3D)
+        if (Mesh is null)
+        {
+            warnings.Add("Mesh is null");
+        }
+        else if (Mesh is not MeshInstance2D and not MeshInstance3D)
+        {
             warnings.Add("Mesh is not a MeshInstance2D or MeshInstance3D");
+        }
 
-        if (Shader is null) warnings.Add("Shader is null");
+        if (Shader is null)
+        {
+            warnings.Add("Shader is null");
+        }
 
-        if (Prop is null) warnings.Add("Prop is null");
+        if (Prop is null)
+        {
+            warnings.Add("Prop is null");
+        }
         else if (Prop is not Interactable)
+        {
             warnings.Add("Prop is not an Interactable");
+        }
 
         return warnings.ToArray();
     }
 
     private void InitializeMesh()
     {
-        if (Mesh is MeshInstance2D) _mesh2D = Mesh as MeshInstance2D;
-        else if (Mesh is MeshInstance3D) _mesh3D = Mesh as MeshInstance3D;
+        if (Mesh is MeshInstance2D)
+        {
+            _mesh2D = Mesh as MeshInstance2D;
+        }
+        else if (Mesh is MeshInstance3D)
+        {
+            _mesh3D = Mesh as MeshInstance3D;
+        }
     }
 
     private void ShowHighlighter()
     {
-        if (!Enabled) return;
+        if (!Enabled)
+        {
+            return;
+        }
 
         if (_mesh2D is not null && _mesh2D.Material is null)
+        {
             _mesh2D.Material = Shader;
+        }
         else if (_mesh3D is not null && _mesh3D.MaterialOverlay is null)
+        {
             _mesh3D.MaterialOverlay = Shader;
+        }
     }
 
     private void HideHighlighter()
     {
-        if (_mesh2D is not null) _mesh2D.Material = null;
-        else if (_mesh3D is not null) _mesh3D.MaterialOverlay = null;
+        if (_mesh2D is not null)
+        {
+            _mesh2D.Material = null;
+        }
+        else if (_mesh3D is not null)
+        {
+            _mesh3D.MaterialOverlay = null;
+        }
     }
 
     private void OnFocus(Interactor prop)
     {
-        if (HighlightOn == EHighlightOn.Focus) ShowHighlighter();
+        if (HighlightOn == EHighlightOn.Focus)
+        {
+            ShowHighlighter();
+        }
     }
 
     private void OnFocusLost(Interactor prop)
     {
-        if (HighlightOn == EHighlightOn.Focus) HideHighlighter();
+        if (HighlightOn == EHighlightOn.Focus)
+        {
+            HideHighlighter();
+        }
     }
 
     private void OnClosest(Interactor prop)
     {
-        if (HighlightOn == EHighlightOn.Closest) ShowHighlighter();
+        if (HighlightOn == EHighlightOn.Closest)
+        {
+            ShowHighlighter();
+        }
     }
 
     private void OnNotClosest(Interactor prop)
     {
-        if (HighlightOn == EHighlightOn.Closest) HideHighlighter();
+        if (HighlightOn == EHighlightOn.Closest)
+        {
+            HideHighlighter();
+        }
     }
 }
