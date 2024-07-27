b87b89b6e38e2eb32c5e2a5e02955077e20b3748
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-07-27 15:31:58 +0200

    feat: add support for operation from custom locations

--- addons/interaction_system/InteractionSystem.cs
+++ addons/interaction_system/InteractionSystem.cs
@@ -6,64 +6,71 @@ namespace InteractionSystem;
 [Tool]
 public partial class InteractionSystem : EditorPlugin
 {
-    private const string commonPath = "res://addons/interaction_system/src/";
-
     public override void _EnterTree()
     {
-        var icon = GD.Load<Texture2D>("res://addons/interaction_system/assets/textures/icon.png");
+        string pluginPath = GetPluginPath();
+        Texture2D icon = GD.Load<Texture2D>(pluginPath + "/assets/textures/icon.png");
 
         AddCustomType(
             "Interactable2D",
             "Node",
-            GD.Load<Script>(commonPath + "interactable/Interactable2D.cs"),
+            GD.Load<Script>(pluginPath + "/src/interactable/Interactable2D.cs"),
             icon
         );
         AddCustomType(
             "Interactable3D",
             "Node",
-            GD.Load<Script>(commonPath + "interactable/Interactable3D.cs"),
+            GD.Load<Script>(pluginPath + "/src/interactable/Interactable3D.cs"),
             icon
         );
         AddCustomType(
             "Interactor2D",
             "Node",
-            GD.Load<Script>(commonPath + "interactor/Interactor2D.cs"),
+            GD.Load<Script>(pluginPath + "/src/interactor/Interactor2D.cs"),
             icon
         );
         AddCustomType(
             "Interactor3D",
             "Node",
-            GD.Load<Script>(commonPath + "interactor/Interactor3D.cs"),
+            GD.Load<Script>(pluginPath + "/src/interactor/Interactor3D.cs"),
             icon
         );
         AddCustomType(
             "CharacterInteractor2D",
             "Node",
-            GD.Load<Script>(commonPath + "interactor/character_interactor/CharacterInteractor2D.cs"),
+            GD.Load<Script>(
+                pluginPath + "/src/interactor/character_interactor/CharacterInteractor2D.cs"
+            ),
             icon
         );
         AddCustomType(
             "CharacterInteractor3D",
             "Node",
-            GD.Load<Script>(commonPath + "interactor/character_interactor/CharacterInteractor3D.cs"),
+            GD.Load<Script>(
+                pluginPath + "/src/interactor/character_interactor/CharacterInteractor3D.cs"
+            ),
             icon
         );
         AddCustomType(
             "InteractableOutlineComponent",
             "Node",
-            GD.Load<Script>(commonPath + "components/interactable_outline_component/InteractableOutlineComponent.cs"),
+            GD.Load<Script>(
+                pluginPath + "/src/components/interactable_outline_component/InteractableOutlineComponent.cs"
+            ),
             icon
         );
         AddCustomType(
             "InteractableHighlighterComponent",
             "Node",
-            GD.Load<Script>(commonPath + "components/interactable_highlighter_component/InteractableHighlighterComponent.cs"),
+            GD.Load<Script>(
+                pluginPath + "/src/components/interactable_highlighter_component/InteractableHighlighterComponent.cs"
+            ),
             icon
         );
         AddCustomType(
             "MouseInteractor2D",
             "Node",
-            GD.Load<Script>(commonPath + "interactor/MouseInteractor2D.cs"),
+            GD.Load<Script>(pluginPath + "/src/interactor/MouseInteractor2D.cs"),
             icon
         );
     }
@@ -83,6 +90,11 @@ public partial class InteractionSystem : EditorPlugin
         RemoveCustomType("InteractableHighlighterComponent");
 
         RemoveCustomType("MouseInteractor2D");
+    }
+
+    private string GetPluginPath()
+    {
+        return GetScript().As<Script>().ResourcePath.GetBaseDir();
     }
 }
 #endif
